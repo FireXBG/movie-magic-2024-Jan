248689bd6ad221bfa359db971a98b92485164170
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-01-22 22:29:14 +0200

    Added src and render home page

--- src/index.js
+++ src/index.js
@@ -21,6 +21,8 @@ app.get("/", (req, res) => {
 
 app.set("view engine", "hbs");
 
+app.use(express.static(path.join(__dirname, "public")));
+
 app.listen(3000, () => {
   console.log(`Server is on post ${port}`);
 });
