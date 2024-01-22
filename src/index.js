6d6a4b745d920c1c3d7822ded821caea7742a88c
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-01-22 21:43:50 +0200

    Created a server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+const express = require("express");
+
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log(`Server is on post ${port}`);
+});
