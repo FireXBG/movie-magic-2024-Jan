39d2c5ba16d0f277e8ac0d75c0658bd3233ce625
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-01-24 19:37:59 +0200

    Adds create movie page

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const homeController = require("./controllers/homeController");
+const movieController = require("./controllers/movieController.js");
 
-router.use("/", homeController);
+router.use(homeController);
+router.use(movieController);
 
 module.exports = router;
