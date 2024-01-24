18646f2b040be1718eb4c5b99a05d0d2cf9c180a
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-01-25 01:02:09 +0200

    Object movies bug fix

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,17 +1,19 @@
-const movies = {
-  title: "Jungle Cruise",
-  genre: "Adventure",
-  director: "Spillberg",
-  date: "2019",
-  imageUrl: "/img/jungle-cruise.jpeg",
-  rating: "5",
-  description: `The youngest of King Triton's daughters, Ariel is
+const movies = [
+  {
+    title: "Jungle Cruise",
+    genre: "Adventure",
+    director: "Spillberg",
+    date: "2019",
+    imageUrl: "/img/jungle-cruise.jpeg",
+    rating: "5",
+    description: `The youngest of King Triton's daughters, Ariel is
     a beautiful and spirited young mermaid with a thirst for
     adventure. Longing to find out more about the world beyond the
     sea, Ariel visits the surface and falls for the dashing Prince
     Eric. Following her heart, she makes a deal with the evil sea
     witch, Ursula, to experience life on land.`,
-};
+  },
+];
 
 exports.create = (movieData) => {
   console.log(movieData);
