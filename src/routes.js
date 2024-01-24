938d8f54713c9608f8c53d88f379d2417b048c4f
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-01-24 20:37:31 +0200

    Ads 404 page

--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,8 @@ const movieController = require("./controllers/movieController.js");
 router.use(homeController);
 router.use(movieController);
 
+router.get("*", (req, res) => {
+  res.render("404");
+});
+
 module.exports = router;
