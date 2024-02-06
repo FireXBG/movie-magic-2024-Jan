4b7e9cbbcab1e7930fccfd6ac89a85a9e2f25a10
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-02-06 03:52:48 +0200

    Add rePass validation

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require("mongoose");
+const { Schema, model, MongooseError } = require("mongoose");
 const bcrypt = require("bcrypt");
 
 const userSchema = new Schema({
@@ -16,6 +16,14 @@ const userSchema = new Schema({
 userSchema.pre("save", async function () {
   const hash = (this.password = await bcrypt.hash(this.password, 12));
   this.password = hash;
+});
+
+userSchema.virtual("rePassword").set(function (value) {
+  // Validate password
+
+  if (value !== this.password) {
+    throw new MongooseError("Password missmatch!");
+  }
 });
 
 const user = model("User", userSchema);
