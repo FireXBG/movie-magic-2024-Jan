de50f3488464fcec9f74346aaa68cb22b9784539
FireXBG <55113543+FireXBG@users.noreply.github.com>
2024-02-09 19:22:25 +0200

    Add edit movie page

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -51,7 +51,9 @@ router.post("/movies/:id/attach", async (req, res) => {
 });
 
 router.get("/movies/:movieId/edit", async (req, res) => {
-  res.render("movie/edit");
+  const movie = await movieService.getOne(req.params.movieId).lean();
+
+  res.render("movie/edit", { movie });
 });
 
 module.exports = router;
